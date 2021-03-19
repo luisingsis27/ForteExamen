6b431e37826f306089670e7387192b1e901c9b2c
luis_ingsis27 <luis_ingsis27@hotmail.com>
2021-03-18 19:29:15 -0600

    proyecto
    proyecto

---
+++ ForteExamen/Views/Cells/ClientesCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ForteExamen.Views.Cells
+{
+    public partial class ClientesCell : ContentView
+    {
+        public ClientesCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
